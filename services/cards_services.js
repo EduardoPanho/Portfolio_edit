634d00d20da1ce38e5d12b4ebbf2cb8cd746b7ff
Eduardo Panho - 에드워드 <eduardopanho9@gmail.com>
2025-06-11 15:55:11 -0300

    barra

--- services/cards_services.js
+++ services/cards_services.js
@@ -1,4 +1,4 @@
-const API_URL = "https://back-end-teal-rho.vercel.app/";
+const API_URL = "https://back-end-teal-rho.vercel.app";
 
 // 🔹 GET – Buscar todos os projetos
 export async function getBdCardsProjects() {
